1e1be1d66d7e0ecea192c92b872c798334397b71
sungkihwan <singahu@naver.com>
2021-09-24 09:00:03 +0900

    server setting done

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"))
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
+
+// node 설치
+// npm i nodemon -D
+// npm i @babel/core @babel/cli @babel/node @babel/preset-env -D
+// npm i express
+// npm instaill pug
+// git init
+// npm run dev
